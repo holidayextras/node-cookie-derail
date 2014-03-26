e42f2e403927ca9806e23faeedfe6b9fbd5facf4
Ollie Buck <ollie.buck@holidayextras.com>
2014-03-26 12:49:20 +0000

    test case for verifying signature of unencrypted cookie

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
+var marsha = require('marsha');
+var crypto = require('crypto');
+
+var _splitCookie = function(input, options, callback) {
+  options = options || {};
+  var components = input.split('--');
+  return callback(components[0], components[1], options);
+};
+
 module.exports = {
-  decode: function(input) {
-    return input;
+  decode: function(input, options) {
+    return _splitCookie(input, options, function(data, signature, options) {
+      if(options.verifySignature === false) {
+        return marsha.load(data, 'base64');
+      } else {
+        var secret = options.secret;
+
+        if(!secret) throw new Error('No secret token provided');
+
+        var hmac = crypto.createHmac('sha1', secret);
+        hmac.update(data);
+        var digest = hmac.digest('hex');
+
+        if(digest != signature) {
+          throw new Error('Signature verification failed');
+        }
+
+        return marsha.load(data, 'base64');
+      }
+    });
+  },
+  decodeEncrypted: function(input, options) {
+    return _splitCookie(input, options, function(data, signature, options) {
+      if(options.verifySignature === false) {
+        return marsha.load(data, 'base64');
+      } else {
+        var secret = options.secret;
+
+        throw new Error('Not implemented yet');
+
+        var iterations = options.iterations || 1000;
+        var salt = options.salt || 'encrypted cookie';
+        var signed_salt = options.signed_salt || 'signed encrypted cookie';
+
+        var secret = crypto.pbkdf2Sync(data, salt, iterations, 32);
+        var signedSecret = crypto.pbkdf2Sync(data, signed_salt, iterations, 64);
+
+        var hmac = crypto.createHmac('sha1', signedSecret);
+        hmac.update(data);
+        console.error(hmac.digest('hex'), signature);
+      }
+    });
   }
 };
--- test/test.js
+++ test/test.js
@@ -1,4 +1,37 @@
 var derail = require('../');
-var example = 'BAh7CEkiD3Nlc3Npb25faWQGOgZFVEkiJWNmNmRiMGFhODcyOTU2MDk3YjYyMDdmMjU0M2ViMmIzBjsAVEkiEHRhbF9zZXNzaW9uBjsARkkiFWIwNDM2MGRkNWU1ODBhMmUGOwBGSSIZd2FyZGVuLnVzZXIudXNlci5rZXkGOwBUWwdbBmkCqwdJIiIkMmEkMTAkYk44ZC5UWFVXcHNiVnA2QzFUM3IwdQY7AFQ%3D--c4fb2f5bbdd0e3f51c6e16e23bd89627277da898'
+var assert = require('assert');
 
-console.log(derail.decode(example));
+var example = 'BAh7CEkiD3Nlc3Npb25faWQGOgZFVEkiJTg5ZTBiODFmZWQxNmQ1MzA2NjczYmY1MjU1NDk5NmQ1BjsAVEkiEHRhbF9zZXNzaW9uBjsARkkiFTRhMjZkZGRiMjM5M2E3YzUGOwBGSSIZd2FyZGVuLnVzZXIudXNlci5rZXkGOwBUWwdbBmkKSSIiJDJhJDEwJDdJdXNyWjNhSXVuWllBREVOVDhQRXUGOwBU--95869713f1a59cb37a6565d029b9a2c91d76d9bb';
+var SECRET_TOKEN = '3d86a6aaf488325927777abf3cdddd5d882a2ab2b364dd98c505336ecdfaf722b54593f4d87c71ff5248634c9f13025e681f829b61665fa65747efb618c93cc5';
+
+describe('cookie-derail', function() {
+
+  it('throws an error if no secret token is passed', function() {
+    assert.throws(function() {
+      derail.decode(example);
+    }, /no secret token provided/i)
+  });
+
+  it('throws an error if an invalid secret token is passed', function() {
+    assert.throws(function() {
+      derail.decode(example, { secret: 'abababbabababababbabababbababababbabababababbaba' });
+    }, /signature verification failed/i)
+  });
+
+  it('deliberately does not verify the cookie if an option is passed', function() {
+    assert.deepEqual(derail.decode(example, { verifySignature: false }), {
+      session_id: '89e0b81fed16d5306673bf52554996d5',
+      tal_session: '4a26dddb2393a7c5',
+      'warden.user.user.key': [[5], '$2a$10$7IusrZ3aIunZYADENT8PEu']
+    });
+  });
+
+  it('reads and returns the value if the correct signature is passed', function() {
+    assert.deepEqual(derail.decode(example, { secret: SECRET_TOKEN }), {
+      session_id: '89e0b81fed16d5306673bf52554996d5',
+      tal_session: '4a26dddb2393a7c5',
+      'warden.user.user.key': [[5], '$2a$10$7IusrZ3aIunZYADENT8PEu']
+    });
+  });
+
+});
